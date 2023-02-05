3e897becaf864c531c868c2051b14d96e8da87c7
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-02-04 19:00:17 -0700

    updates

---
+++ data.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+var questions = [ 
+    {
+       
+        question: "what is an object?",
+        choices:  [
+             "key:value pair",
+             "An unordered collection of key:value pairs",
+             "A property"
+        ],
+        correctAnswer: 1
+    }, 
+    {
+        question: "what is ECMAscript?",
+        choices:  [
+             "European Computer Manufacturer's Association script that defines the standard for interoperability of webpages across different browsers.",
+             "API for manipulating HTML documents. ",
+             "Array like notation parameters"
+            
+    ],
+        correctAnswer: 0
+    },
+    { 
+        question: "What is function hoisting?",
+        choices: [
+             "Boolean syntax",
+             "nullish coalescing assignment operator",
+             " A mechanism that javascript engine physically moves function declarartions to the top of the code before executing them. "
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "How is the pascal case formatted for variable names?",
+        choices: [
+             "vArIaBlEnAmE",
+             "VariableName",
+             " variableName"
+        ],
+            correctAnswer: 1
+            
+
+    },
+    { 
+        question: "What is the difference between var and let?",
+        choices: [ 
+             "let variables are only available inside the block where they're defined whereas var variables are available throughout the function in which they are declared.",
+             " let can carry multiple objects",
+             "var will cannot be redefined."
+        ],
+        correctAnswer: 0
+    },
+    { question: "All Done!",
+        choices: [  "Your final score is: "
+                ],
+                correctAnswer: 1
+
+    }
+]
+
