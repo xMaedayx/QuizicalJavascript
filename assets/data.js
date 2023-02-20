56a063557fe7ac38df854b165a8bd74d7223f142
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-02-20 14:23:53 -0700

    updates

--- assets/data.js
+++ assets/data.js
@@ -10,37 +10,41 @@ var questions = [
         choices:  [
              "key:value pair",
              "An unordered collection of key:value pairs",
-             "A property"
+             "A property",
+             "a person place or thing."
         ],
-        correctAnswer: 1
+        correctAnswer: "key:value pair"
     }, 
     {
         question: "what is ECMAscript?",
         choices:  [
              "European Computer Manufacturer's Association script that defines the standard for interoperability of webpages across different browsers.",
              "API for manipulating HTML documents. ",
-             "Array like notation parameters"
+             "Array like notation parameters",
+             "Elegant Court Martials Alliance and the script for their meetings."
             
     ],
-        correctAnswer: 0
+        correctAnswer: "European Computer Manufacturer's Association script that defines the standard for interoperability of webpages across different browsers."
     },
     { 
         question: "What is function hoisting?",
         choices: [
              "Boolean syntax",
              "nullish coalescing assignment operator",
-             " A mechanism that javascript engine physically moves function declarartions to the top of the code before executing them. "
+             "A mechanism that javascript engine physically moves function declarartions to the top of the code before executing them.",
+             "A series of bungee cords to life the function to the next level."
         ],
-        correctAnswer: 2
+        correctAnswer: "A mechanism that javascript engine physically moves function declarartions to the top of the code before executing them."
     },
     {
         question: "How is the pascal case formatted for variable names?",
         choices: [
              "vArIaBlEnAmE",
              "VariableName",
-             " variableName"
+             "variableName",
+             "Blaise Pascal; French mathematician, physicist, inventor, philosopher, and Catholic writer "
         ],
-            correctAnswer: 1
+            correctAnswer: "VariableName"
             
 
     },
@@ -49,15 +53,10 @@ var questions = [
         choices: [ 
              "let variables are only available inside the block where they're defined whereas var variables are available throughout the function in which they are declared.",
              " let can carry multiple objects",
-             "var will cannot be redefined."
+             "var will cannot be redefined.",
+             "varsity of lettuce"
         ],
-        correctAnswer: 0
-    },
-    { question: "All Done!",
-        choices: [  "Your final score is: "
-                ],
-                correctAnswer: 1
-
+        correctAnswer: "let variables are only available inside the block where they're defined whereas var variables are available throughout the function in which they are declared."
     }
 ]
 
--- assets/script.js
+++ assets/script.js
@@ -1,88 +1,297 @@
 var Time = document.querySelector("#Time")
 var startButton = document.querySelector("#start-button")
-var buttons = document.querySelector("#multipleChoice")
-var area = document.querySelector(".area-div");
+var buttons = document.querySelector(".multipleChoice")
+var button1 = document.querySelector("op1")
+var button2 = document.querySelector("op2")
+var button3 = document.querySelector("op3")
+var button4 = document.querySelector("op4")
 var questionEl = document.querySelector("#question")
-var quizBox = document.querySelector(".area")
+var quizBox = document.querySelector("#container")
+var questionBox = document.querySelector("#question-container")
 var displayEl = document.querySelector("#display")
-var shuffledQuestions, currentQuestionsIndex
+var startGuide = document.querySelector("#startPage")
+var initials = document.querySelector("#InputInitials")
+var scoreTally = document.querySelector("#win-loss-counter")
 
+var shuffledQuestions, currentQuestionsIndex
+var totalTime = 60;
 
 
 
 function startgame() {
     startButton.classList.add('hide')
-    quizBox.classList.remove('hide')
+    questionBox.classList.remove('hide')
     shuffledQuestions = questions.sort(() => Math.random() -0.5)
     currentQuestionsIndex = 0
     displayQuestions()
+    timer()
 }
 
 function timer() {
-  var totalTime = 60;
-  var timeInterval = setInterval(function() {
+  let timeInterval = setInterval(function() {
       if (totalTime >= 1) {
        Time.textContent = totalTime + 'seconds'; 
        totalTime --;
       }
       else { 
-          Time.textContent = 'NEXT'; 
           clearInterval(timeInterval);
+        displayClose()  
           
       }
   }, 1000);
 }
 
 
-
-
-function displayQuestions() { 
-    buttons.addEventListener("click", function() {
-        if (buttons.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
-          displayEl.innerText = "Correct!";
-          setNextQuestion();
-        } else {
-          displayEl.innerText = "Incorrect!";
-          Time.innerText = totalTime.innerText - 10;
-        }
-      });
-      
-      buttons.addEventListener("click", function() {
-        if (buttons.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
-          displayEl.innerText = "Correct!";
-          setNextQuestion();
-        } else {
-          displayEl.innerText = "Incorrect!";
-          Time.innerText = totalTime.innerText - 10;
-        }
-      });
-      
-      buttons.addEventListener("click", function() {
-        if (buttons.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
-          displayEl.innerText = "Correct!";
-          setNextQuestion();
-        } else {
-          displayEl.innerText = "Incorrect!";
-          Time.innerText = totalTime.innerText - 10;
-        }
-      });
-      
-      buttons.addEventListener("click", function() {
-        if (buttons.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
-          displayEl.innerText = "Correct!";
-          setNextQuestion();
-        } else {
-          displayEl.innerText = "Incorrect!";
-          Time.innerText = totalTime.innerText - 10;
-        }
-      });
-      }
-
-      function setNextQuestion() { 
-    currentQuestionsIndex++;
-    if (currentQuestionsIndex === shuffledQuestions.length) {
-   // set as the end where there is a prompt for initials. displayEl.innerText =
-  } else {
-    displayQuestions();
-      }
+function displayQuestions() {
+    questionEl.innerText = shuffledQuestions[currentQuestionsIndex].question;
+    
+    const choices = shuffledQuestions[currentQuestionsIndex].choices;
+    for (let i = 0; i < choices.length; i++) {
+      const button = document.getElementById(`.op${i + 1}`);
+      button.innerText = choices[i];
     }
+  }
+    button1.addEventListener("click", function() {
+        if (button1.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
+          displayEl.innerText = "Correct!";
+          setNextQuestion();
+        } else {
+          displayEl.innerText = "Incorrect!";
+          totalTime -= 10
+        }
+      });
+      button2.addEventListener("click", function() {
+        if (button2.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
+          displayEl.innerText = "Correct!";
+          setNextQuestion();
+        } else {
+          displayEl.innerText = "Incorrect!";
+          totalTime -= 10
+        }
+      });
+        button3.addEventListener("click", function() {
+          if (button3.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
+            displayEl.innerText = "Correct!";
+            setNextQuestion();
+          } else {
+            displayEl.innerText = "Incorrect!";
+            totalTime -= 10
+          }
+        });
+        button4.addEventListener("click", function() {
+          if (button4.innerText === shuffledQuestions[currentQuestionsIndex].correctAnswer) {
+            displayEl.innerText = "Correct!";
+            setNextQuestion();
+          } else {
+            displayEl.innerText = "Incorrect!";
+            totalTime -= 10
+          }
+       });
+      
+      
+      
+        function setNextQuestion()  { 
+          showQuestion(shuffledQuestions[currentQuestionsIndex])
+        }
+
+        function showQuestion() { 
+          questionEl.innerText = questions.question;
+        }
+      startButton.addEventListener("click", startgame);
+      buttons.addEventListener("click", displayQuestions)
+     
+
+      
+    
+      // window.location.redriect moving one page to another// 
+      // 
+
+
+/* function rightAnswer(
+let answerTrue = questions[index].correctAnswer
+
+}
+
+/* startButton.addEventListener("click", startgame);
+button1.addEventListener("click", displayQuestions);
+button2.addEventListener("click", displayQuestions);
+button3.addEventListener("click", displayQuestions);
+button4.addEventListener("click", displayQuestions);
+
+
+
+
+
+
+// event listener on each button clicked grab text of button 
+//check to see if its a right answer
+// timer 
+// increment value of currentquestion index 
+//
+
+
+
+
+
+
+
+
+
+/* if (choices.correct) {
+    button1.dataset.correct = choices.correctAnswer
+  }
+  button1.addEventListener('click', choices.correctAnswer)
+  button1.appendChild(button1)
+
+  if (choices.correct) {
+    button2.dataset.correct = choices.correctAnswer
+  }
+  button2.addEventListener('click', choices.correctAnswer)
+  button2.appendChild(button2)
+
+
+if (choices.correct) {
+    button3.dataset.correct = choices.correctAnswer
+  }
+  button3.addEventListener('click', choices.correctAnswer)
+  button3.appendChild(button3)
+    
+    
+if (choices.correct) {
+    button3.dataset.correct = choices.correctAnswer
+  }
+  button3.addEventListener('click', choices.correctAnswer)
+  button3.appendChild(button3)
+
+if (choices.correct) {
+    button4.dataset.correct = choices.correctAnswer
+  }
+  button4.addEventListener('click', choices.correctAnswer)
+  button4.appendChild(button4)
+
+
+
+
+
+
+
+
+
+
+
+
+/* var winCounter = 0;
+var lossCounter = 0; 
+
+
+
+
+function timer() {
+    var totalTime = 60;
+    var timeInterval = setInterval(function() {
+        if (totalTime >= 1) {
+         Time.textContent = totalTime + 'seconds'; 
+         totalTime --;
+        }
+        else { 
+            Time.textContent = 'NEXT'; 
+            clearInterval(timeInterval);
+            
+        }
+    }, 1000);
+}
+
+
+function displayQuestion (questionObj) {
+    console.log(questionObj)
+    questionEl.textContent = questionObj.question
+    for (i=0; i <= questionObj.choices.length; i++) {
+        var choiceEl = document.createElement("button")
+        choiceEl.textContent = questionObj.choices[i]
+        choiceEl.setAttribute("value", questionObj.choices[i])
+        multipleChoice.appendChild(choiceEl)
+
+    }
+
+   
+    
+}
+
+function RightAnswer (userInput) {
+    let answerTrue = questions[index].correctAnswer
+    displayEl.getElementsByClassName.visibility = "visible"
+    
+    
+}
+
+
+startButton.addEventListener("click", function() {
+    displayQuestion(questions[i]) 
+    timer()
+
+} )
+
+function enterInitials() { 
+   UserInputInitials.appendChild() 
+
+}
+
+function highscores() { 
+    if (correctAnswer === true){
+        winCounter++ 
+        localStorage.setItem("winCount", winCounter);
+     }
+     if (correctAnswer === false) {
+        lossCounter++
+        localStorage.setItem("winCount", winCounter)
+     } 
+} 
+
+
+
+
+
+ button1.addEventListener("click", function() {
+    return rightAnswer(correctAnswer, currentQuestion);
+  });
+  button2.addEventListener("click", function() {
+    return rightAnswer(correctAnswer, currentQuestion);
+  });
+  button3.addEventListener("click", function() {
+    return rightAnswer(correctAnswer, currentQuestion);
+  });
+  button4.addEventListener("click", function() {
+    return rightAnswer(correctAnswer, currentQuestion);
+  });
+
+
+
+
+
+
+
+
+
+
+ //logs window object
+console.log(window)
+
+//logs reference to the document in the window object 
+console.log(window.document)
+
+//logs root element 
+ console.log(document.documentElement)
+
+ // Returns head element of current document
+ console.log(document.head)
+
+ 
+ questionEl.innerText = shuffledQuestions[currentQuestionsIndex].question
+button1.innerText = shuffledQuestions[currentQuestionsIndex].choices[0]
+button2.innerText = shuffledQuestions[currentQuestionsIndex].choices[1]
+button3.innerText = shuffledQuestions[currentQuestionsIndex].choices[2]
+button4.innerText = shuffledQuestions[currentQuestionsIndex].choices[3]
+
+ 
+ 
+ */ 
